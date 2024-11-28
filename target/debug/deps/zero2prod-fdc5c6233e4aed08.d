b5ca4fee9dbc91a30e119dfb4b7f3b2136236547
Miguel Lopez <miguellova@gmail.com>
2024-11-27 22:51:29 -0600

    Reapply gitignore file to remove 'target'

--- target/debug/deps/zero2prod-fdc5c6233e4aed08.d
+++
@@ -1,9 +0,0 @@
-/home/valdev/personal/rust-projects/rust-learning/zero2prod/target/debug/deps/libzero2prod-fdc5c6233e4aed08.rmeta: src/main.rs Cargo.toml
-
-/home/valdev/personal/rust-projects/rust-learning/zero2prod/target/debug/deps/zero2prod-fdc5c6233e4aed08.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- target/debug/zero2prod.d
+++
@@ -1,1 +0,0 @@
-/home/valdev/personal/rust-projects/zero2prod/target/debug/zero2prod: /home/valdev/personal/rust-projects/zero2prod/src/main.rs
